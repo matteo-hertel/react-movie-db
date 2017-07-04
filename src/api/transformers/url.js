515a133319fb72d490fcc63ed38405725e084616
Matteo Hertel <info@matteohertel.com>
2017-07-04 16:23:13 +0200

    add all the common query parameters needed for the movie db

--- src/api/transformers/url.js
+++ src/api/transformers/url.js
@@ -14,8 +14,59 @@
  * @param {string} url 
  * @param {string} api_key 
  * @param {boolean} [first_chain=false] 
- * @returns 
+ * @returns string
  */
 export function addApiKey(url, api_key, first_chain = false) {
     return `${url}${first_chain ? "?" : "&"}api_key=${api_key}`;
+}
+/**
+ * same principle as above
+ * 
+ * @export
+ * @param {string} url 
+ * @param {string} query
+ * @param {boolean} [first_chain=false] 
+ * @returns string
+ */
+export function addQuery(url, query, first_chain = false) {
+    return `${url}${first_chain ? "?" : "&"}query=${encodeURIComponent(query)}`;
+}
+
+//All the default values should be in some sort of config this is just a PoC
+
+/**
+ * same principle as above
+ * 
+ * @export
+ * @param {string} url 
+ * @param {string} [filter="false"] 
+ * @param {boolean} [first_chain=false] 
+ * @returns string
+ */
+export function addAdultFilter(url, filter = "false", first_chain = false) {
+    return `${url}${first_chain ? "?" : "&"}include_adult=${filter}`;
+}
+/**
+ * same principle as above
+ * 
+ * @export
+ * @param {string} url 
+ * @param {string|int} [page="1"] 
+ * @param {boolean} [first_chain=false] 
+ * @returns string
+ */
+export function addPagination(url, page = "1", first_chain = false) {
+    return `${url}${first_chain ? "?" : "&"}page=${page}`;
+}
+/**
+ * same principle as above
+ * 
+ * @export
+ * @param {string} url 
+ * @param {string} [language="en-GB"] 
+ * @param {boolean} [first_chain=false] 
+ * @returns string
+ */
+export function addLanguage(url, language = "en-GB", first_chain = false) {
+    return `${url}${first_chain ? "?" : "&"}language=${language}`;
 }
