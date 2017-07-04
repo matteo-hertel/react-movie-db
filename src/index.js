788b25aa84acec40c83c6bb46006112df7765b95
Matteo Hertel <info@matteohertel.com>
2017-07-04 23:05:28 +0200

    defined dependencies in the index

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//grab all the dependencies and inject them through the components for them to use em
+import config from "./config";
+import { MovieDb } from "./api/moviedb";
+import { enrichSearchResults } from "./api/transformers/data";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const dataEnricher = enrichSearchResults(config);
+const movieAPI = MovieDb(config);
+
+ReactDOM.render(<App moviedb={movieAPI} dataEnricher={dataEnricher} />, document.getElementById('root'));
