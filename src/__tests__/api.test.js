b1bce0d14a13c55d8faf30a217cd2574eb5c6e08
Matteo Hertel <info@matteohertel.com>
2017-07-04 15:48:12 +0200

    add high level overview of api functionnality

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -1,6 +1,7 @@
 import { MovieDb } from "./../api/moviedb";
 
 const MovieAPI = new MovieDb();
+const query = encodeURIComponent("Wonder Woman");
 /**
  * Test the high level module in order to have test something ready for production
  * if I have spare time I'll be able to test every single bit
@@ -9,5 +10,9 @@ describe("The high level module", function () {
 
     it('should return an object', () => {
         expect(typeof MovieAPI).toEqual("object"); //I'm sure there is a better way to do this but for now will do
+    });
+
+    it('should have a searchMovie function that returns a promise', () => {
+        expect(typeof MovieAPI.searchMovies(query)).toEqual("function"); //I'm sure there is a better way to do this but for now will do
     });
 })
--- src/api/moviedb.js
+++ src/api/moviedb.js
@@ -11,7 +11,34 @@
  */
 export function MovieDb(config) {
     return {
-        searchMovies: function () { },
+        searchMovies: searchMovies(config), //the search movie is exportable but I'll inject the configs so this module is ready to use
         getMovie: function () { }
     };
+}
+/**
+ * @export
+ * @param {any} config requires baseUrl and apiKey
+ * this will make this function working with different configs and be highly reusable
+ * @returns function
+ */
+export function searchMovies(config) {
+    /**
+     * in here maybe a check to see if the required configs are there
+     * it's a super small app so for now will skip that just add a comment in the
+     * docs above
+     */
+
+    /**
+     * Name your anonymous functions, it will be extremely helpful while debugging
+     * 
+     * search function will accept a query in a return a promise (using fetch) to resolve the 
+     * HTTP call
+     * 
+     * @param string query
+     * @return Promise
+     */
+    return function search(query) {
+        let url = `${config.baseUrl}/search/movie&api_key=${config.apiKey}&query=${query}`;
+        return fetch(url)
+    }
 }
