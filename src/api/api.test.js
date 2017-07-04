cf63a06293de334e2fe02991cef54ca0570abfec
Matteo Hertel <info@matteohertel.com>
2017-07-04 15:33:02 +0200

    add high level API module with test

---
+++ src/api/api.test.js
@@ -0,0 +1,12 @@
+import { MovieDb } from "./../api/moviedb";
+
+const MovieAPI = new MovieDb();
+/**
+ * Test the high level module in order to have test something ready for production
+ * if I have spare time I'll be able to test every single bit
+ */
+describe("The high level module", function () {
+    it('should return an object', () => {
+        expect(typeof MovieAPI).toEqual("object");
+    });
+})
---
+++ src/api/moviedb.js
@@ -0,0 +1,17 @@
+/**
+ * High level function that will return the available function
+ * to query MovieDB API
+ * 
+ * Because of the time constraint I might skip some tests or go
+ * with questionable choices :)
+ * 
+ * @export
+ * @param {any} config injected configs 
+ * @returns Object
+ */
+export function MovieDb(config) {
+    return {
+        searchMovies: function () { },
+        getMovie: function () { }
+    };
+}
