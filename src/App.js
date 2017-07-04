89837413eb1cabb54263fbf8738e41630144e4f4
Matteo Hertel <info@matteohertel.com>
2017-07-04 23:13:03 +0200

    add high level how the app should behave

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const state = {
+    searchTerm: ""
+}
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = state;
+    }
     render() {
         return (
             <div className="App">
                 <div className="App-header">
                     <h2>MovieDB App</h2>
                 </div>
-                <p className="App-intro">
-                    add here your movies
-                </p>
+                <div className="App-intro">
+                    <div>Search Bar</div>
+                    <div>Search Results</div>
+                </div>
             </div>
         );
     }
